711a1c2f62d70cd6aafa0d930814ef7f6d7d51a7
Alicia Quezada <aqueza2@gmail.com>
2016-02-19 09:44:24 -0600

    remove timestamps from relationship types, languages, questions, skills, gender identities, sexual orientations, sexual preferences, text editors and operating systems tables.

--- db/migrate/20160219012548_questions.rb
+++ db/migrate/20160219012548_questions.rb
@@ -2,8 +2,6 @@ class Questions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :title, null: false
-
-      t.timestamps(null: false)
     end
   end
 end
--- db/migrate/20160219012737_text_editors.rb
+++ db/migrate/20160219012737_text_editors.rb
@@ -2,8 +2,6 @@ class TextEditors < ActiveRecord::Migration
   def change
     create_table :text_editors do |t|
       t.string :name, null: false
-
-      t.timestamps(null: false)
     end
   end
 end
