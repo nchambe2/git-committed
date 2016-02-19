40053e8921f95f7e3d5a8993a3391c329bb5623d
Alicia Quezada & Chris Gomes <pair+aqueza2.ugaliguy@devbootcamp.com>
2016-02-19 10:09:03 -0600

    Add model for skill

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,1 +1,6 @@
-filter.rb
+class Filter < ActiveRecord::Base
+  has_many :user_filters
+
+  validates :filterable_id, presence: true
+  validates :filterable_type, presence: true
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ActiveRecord::Base
-  has_one :answer
+  has_many :answers
   validates :title, presence: true
 end
