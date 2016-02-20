13507def4c8451cd97fefc326baa152b30621475
Nicolette Chambers <nicolette3883@gmail.com>
2016-02-20 15:24:20 -0600

    Add age drop down to filters page

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,5 +1,6 @@
 class FiltersController < ActionController::Base
   def index
+    # @user = User.find(current_user.id)
     @programming_languages = Language.language_names
     @skills = Skill.skill_name
     @sexual_prefences = SexualPreference.sexual_preference_names
