7965f398e1d43d81221d374f82960cf21563b6f0
Alicia Quezada <aqueza2@gmail.com>
2016-02-20 17:16:27 -0600

    add profile routes to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   get '/profile', to: 'profiles#show'
+  get '/profile/edit', to: 'profiles#edit'
 
   get  '/filters', to: 'filters#index', as: 'filters'
   post '/filters', to: 'filters#create'
