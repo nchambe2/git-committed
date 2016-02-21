e17e153a6538fbe9de2e04422b55519946682146
Alicia Quezada & Chris Gomes <pair+aqueza2.ugaliguy@devbootcamp.com>
2016-02-20 19:28:10 -0600

    Created an account settings controller & edit page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
+  get '/account_settings', to: 'account_settings#edit', as: 'account_settings'
+  patch '/account_settings', to: 'account_settings#update'
+
   get '/profile', to: 'profiles#show'
 
   get  '/filters', to: 'filters#index', as: 'filters'
