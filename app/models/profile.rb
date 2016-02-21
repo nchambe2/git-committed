9c4b0448ea34b7e33bd0e23d9a109a3120b8221a
Alicia Quezada <aqueza2@gmail.com>
2016-02-21 15:52:08 -0600

    start users controller testing

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,8 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_many :answers
   has_many :questions, through: :answers
-  
+
   paginates_per 15
+
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,16 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/test/'
+  add_filter '/config/'
+  add_filter '/lib/'
+  add_filter '/vendor/'
+
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Views', 'app/views'
+end
 
 # Previous content of test helper now starts here
 
