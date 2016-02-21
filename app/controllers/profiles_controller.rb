d39fa54e9d34c92b14be695734274ad48f5688cd
Alicia Quezada & Chris Gomes <pair+aqueza2.ugaliguy@devbootcamp.com>
2016-02-20 18:44:59 -0600

    Add profiles_controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   def show
-
+    @profile = Profile.find_by(id: params[:id])
+    @profile.username = User.find_by(id: params[:username])
   end
 
   def edit
