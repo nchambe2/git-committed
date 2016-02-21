1f940b0a486d1aca6be0dfb42053a97bb204dbd7
svcastaneda <svcastaneda@gmail.com>
2016-02-21 11:49:49 -0600

    Create profile on registration. Fix profile URL.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
+      Profile.create(user: @user)
       redirect_to user_path(@user)
     else
       @errors = @user.errors.full_messages
