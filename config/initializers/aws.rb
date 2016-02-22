b4453b29e3c3ba9c65a6ccbd58c2a4c0048d29de
Alicia Quezada <aqueza2@gmail.com>
2016-02-22 10:08:34 -0600

    paperclip config fixes, with mike

--- config/initializers/aws.rb
+++
@@ -1,5 +0,0 @@
-require 'aws-sdk'
-# Rails.configuration.aws is used by AWS, Paperclip, and S3DirectUpload
-Rails.configuration.aws = YAML.load(ERB.new(File.read("#{Rails.root}/config/aws.yml")).result)[Rails.env].symbolize_keys!
-AWS.config(logger: Rails.logger)
-AWS.config(Rails.configuration.aws)
