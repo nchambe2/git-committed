19edcf148cf7739f64759b3f1ba849f303d1bf2a
svcastaneda <svcastaneda@gmail.com>
2016-02-22 08:22:33 -0600

    Wrap groups into fieldsets

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,14 +2,17 @@ class FiltersController < ApplicationController
   def edit
     @languages = Filter.where(filterable_type:"Language")
     @skills = Filter.where(filterable_type:"Skill")
-    @sexual_preferences = SexualPreference.sexual_preference_names
-    
+    @text_editors = Filter.where(filterable_type:"TextEditor")
+    @operating_systems = Filter.where(filterable_type:"OperatingSystem")
+    # @sexual_preferences = Filter.where(filterable_type:"SexualPreference"
+    @sexual_preferences = SexualPreference.pluck("name")
+
     @user_filters = current_user.user_filters
   end
-  
+
   def update
     user_filters = current_user.user_filters
-    
+
     @filters = params[:user_filter]
     @filters.each do |f|
       filter = Filter.find_by(id: f[0])
@@ -19,7 +22,7 @@ class FiltersController < ApplicationController
     end
     redirect_to profile_path(current_user)
   end
-  
+
   private
   def get_value(item)
     item == '1'
