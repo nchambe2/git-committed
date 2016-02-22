caabf297018cf12a428874c08be07d80b36eb787
svcastaneda <svcastaneda@gmail.com>
2016-02-21 20:41:59 -0600

    Add schema thign

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221025429) do
+ActiveRecord::Schema.define(version: 20160222004918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,8 +88,9 @@ ActiveRecord::Schema.define(version: 20160221025429) do
   create_table "user_filters", force: :cascade do |t|
     t.integer  "filter_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.boolean  "active",     default: true
   end
 
   create_table "user_languages", force: :cascade do |t|
