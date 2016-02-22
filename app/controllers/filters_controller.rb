abc9c70a8578f9d27402c494a9521ff81f7ec689
svcastaneda <svcastaneda@gmail.com>
2016-02-21 20:51:34 -0600

    Add updated controllers

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,10 +1,27 @@
 class FiltersController < ApplicationController
   def edit
-    @programming_languages = Language.language_names
-    @skills = Skill.skill_names
+    @languages = Filter.where(filterable_type:"Language")
+    @skills = Filter.where(filterable_type:"Skill")
     @sexual_preferences = SexualPreference.sexual_preference_names
+    
+    @user_filters = current_user.user_filters
   end
   
   def update
+    user_filters = current_user.user_filters
+    
+    @filters = params[:user_filter]
+    @filters.each do |f|
+      filter = Filter.find_by(id: f[0])
+      user_filter = user_filters.find_by(filter: filter)
+      value = get_value(f[1])
+      user_filter.update_attributes(active: value) if user_filter
+    end
+    redirect_to profile_path(current_user)
+  end
+  
+  private
+  def get_value(item)
+    item == '1'
   end
 end
